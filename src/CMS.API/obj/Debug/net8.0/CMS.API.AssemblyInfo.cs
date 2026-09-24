87951bc08de2452e14259410f01b1cbaac8a9da7
Biniyam0911 <161204171+Biniyam0911@users.noreply.github.com>
2026-09-24 19:20:49 +0300

    25

--- src/CMS.API/obj/Debug/net8.0/CMS.API.AssemblyInfo.cs
+++ src/CMS.API/obj/Debug/net8.0/CMS.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f2eceb921efff20ab8938471fec92592dc8d4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20e1f453608ce2d3420f79c2855d482aa5aadce")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
